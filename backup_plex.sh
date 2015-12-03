c664b943ffcd9dc1fbf2425a8ed004595d9e7416
pyro2927 <joseph@pintozzi.com>
2015-12-02 23:12:44 -0600

    Removing cache before tar

--- backup_plex.sh
+++ backup_plex.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pushd /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/
+rm -rf Cache/PhotoTranscoder/*
+rm -rf Cache/Transcode/*
 tar czf ~/plex_media_server_backup_$(date +%Y%m%d-%H%M%S).tar.gz .
